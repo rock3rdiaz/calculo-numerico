1cba5f5b60122b2b90b5a6fdd3a48bac81687e23
Rogers Diaz <roger.diaz@globant.com>
2023-05-20 08:07:32 -0500

    Adicion del bucle principal.

--- actividades/Grupal/src/main.cpp
+++ actividades/Grupal/src/main.cpp
@@ -1,61 +1,26 @@
 #include <iostream>
+#include <ctime>
 #include <array>
 #include "non_lineals.h"
-#include "norma_l1.h"
-#include "inversion.h"
-#include "sustitucion.h"
-#include "lu.h"
 
 using namespace std;
 using namespace non_lineals;
-using namespace norma_l1;
-using namespace sustitucion;
-using namespace inversion;
-using namespace lu;
-
 
 int main() {
 
-    const double e0 { 1.0e-14 }; // valor de tolerancia
-    
-    /*
-    // Declaramos la matriz A como un vector de vectores (2x2)
-	vector<vector<double>> A (2, vector<double>(2));
+    unsigned t0 = clock();
 
-	// Inicializamos A
-	A[0][0] = 1.0; A[0][1] = 1.0;
-	A[1][0] = 3.0; A[1][1] = -1.0;
-    
-    double norma_A = norma_l1::norma_l1(A);
+    cout << "Iniciando proceso ..." << endl;
 
-    // Definimos el vector de terminos independientes
-	vector<double> w = { 9.0, 4.0 };
+    array<double, 2> x0 = { 1.0, 1.0 }; // aproximacion incial
 
-    // Vector de permutaciones y determinante
-	vector<int> perm(2);
-	double det;
+    non_lineals::Newton_nD(x0); // invocacion del metodo principal
 
-	// Descomposicion LU
-    lu::lu(A, perm, det);
-	
-    // Resolucion del sistema de ecuaciones
-    sustitucion::sustitucion(A, perm, w);
-	
-    // Inversion de A
-    inversion::inversion(A, perm);
+    unsigned t1 = clock();
 
-    // Calculamos la norma L1 de la inversa de A
-	double norma_A_inv = norma_l1::norma_l1(A);
+    double time_elapsed = (double(t1 - t0) / CLOCKS_PER_SEC);
 
-	// Obtenemos el numero de condicion
-	double numero_de_condicion = norma_A * norma_A_inv;
-
-	cout << "Numero de condicion: C(A) = " << numero_de_condicion << endl;
-	cout << "Solucion con A: (x = " << w[0] << ", y = " << w[1] << ")" << endl;
-    */
-
-    array<double, 2> x0 = { 1.0, 1.0 };
-    non_lineals::Newton_nD(x0, e0);
+    cout << "Proceso finalizado. Tiempo de procesamiento: " << time_elapsed << " segundos" << endl;
 
     return 0;
 }
--- actividades/Grupal/src/non_lineals.h
+++ actividades/Grupal/src/non_lineals.h
@@ -1,14 +1,31 @@
+/**
+ *
+ * @brief      Este archivo contiene la implementacion del metodo solicitado en la actividad.
+ *             Hace uso de los archivos proporcionados en el tema5 de la asignatura.
+ *             Solamente adicionamos unos header guards y nombres de dominio para evitar
+ *             referencias ciclicas o redefiniciones de los mismos metodos.
+ *
+ * @author     Roger.diaz, Alicia.arellano, Ivan.santiago
+ * @date       2023
+*/
+
 #include <cmath>
 #include <array>
 #include "lu.h"
 #include "sustitucion.h"
+#include "norma_l1.h"
+#include "inversion.h"
 
 #ifndef NON_LINEALS_H
 #define NON_LINEALS
 
 using namespace std;
-using namespace lu;
 using namespace sustitucion;
+using namespace norma_l1;
+using namespace inversion;
+using namespace lu;
+
+const double e0 { 1.0e-14 }; // valor de tolerancia
 
 namespace non_lineals {
 
@@ -39,21 +56,55 @@ namespace non_lineals {
 
     /**
      * Funcion definida en la actividad.
-     * Recibe un puntero a la funcion con la definicion del sistema, el jacobiano del sistema,
-     * el valor de tolerancia y una primera aproximacion de la solucion.
+     * Recibe el punto x0 = (x, y) que sera la aproximacion inicial. 
      *
      */
-    void Newton_nD(array<double, 2> &initial_approx, const double &e0){
-        array<double, 2> _fx { 0.0, 0.0 };
-        array<array<double, 2>, 2> _jfx { { { 0.0, 0.0 }, { 0.0, 0.0 } } };
-        fx(initial_approx[0], initial_approx[1], _fx);
-        jfx(initial_approx[0], initial_approx[1], _jfx);
+    void Newton_nD(array<double, 2> &initial_approx) {
+        array<double, 2> _fx { 0.0, 0.0 }; // referencia a F(x0)
+        array<array<double, 2>, 2> _jfx { { { 0.0, 0.0 }, { 0.0, 0.0 } } }; // referencia a JF(X0)
+        
+        // Declaramos la matriz A como un vector de vectores (2x2)
+        vector<vector<double>> A (2, vector<double>(2));
+        
+        // Definimos el vector de terminos independientes
+        vector<double> w = { 9.0, 4.0 };
+        
+        // Vector de permutaciones y determinante
+        vector<int> perm(2);
+        double det;
+
+        // Inicializamos A
+        A[0][0] = 1.0; A[0][1] = 1.0;
+        A[1][0] = 3.0; A[1][1] = -1.0;
+
+        while (abs(_fx[0]) > e0 and abs(_fx[1]) > e0) {
+            double norma_A = norma_l1::norma_l1(A);
+
+            // Descomposicion LU
+            lu::lu(A, perm, det);
+            
+            // Resolucion del sistema de ecuaciones
+            sustitucion::sustitucion(A, perm, w);
+            
+            // Inversion de A
+            inversion::inversion(A, perm);
+
+            // Calculamos la norma L1 de la inversa de A
+            double norma_A_inv = norma_l1::norma_l1(A);
+
+            // Obtenemos el numero de condicion
+            double numero_de_condicion = norma_A * norma_A_inv;
+
+            cout << "Numero de condicion: C(A) = " << numero_de_condicion << endl;
+            cout << "Solucion con A: (x = " << w[0] << ", y = " << w[1] << ")" << endl;
+ 
+            fx(initial_approx[0], initial_approx[1], _fx);
+            jfx(initial_approx[0], initial_approx[1], _jfx);
+        }
 
         cout << "F(x0) = {" << " x: " << _fx[0] << ", y: " << _fx[1] << " }" << endl;
         cout << "JF(x0) = {" << " x00: " << _jfx[0][0] << ", y01: " << _jfx[0][1]  << ", x10: " << _jfx[1][0] << ", y11: " << _jfx[1][1] << "}" << endl;
-
     }
-
 }
 
 #endif
